bcd2d60177fabbf0f02bf680dd362a181c23d17a
Lucky Victory <luckyvictory54@gmail.com>
2024-11-16 23:25:53 +0100

    added Network banner

--- blog/src/app/components/PageWrapper.tsx
+++ blog/src/app/components/PageWrapper.tsx
@@ -3,14 +3,17 @@ import { PropsWithChildren } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Box, useColorModeValue } from "@chakra-ui/react";
+import NetworkAvailabiltyCheck from "./NetworkAvailabiltyCheck";
 
 export default function PageWrapper({ children }: PropsWithChildren) {
   const bgColor = useColorModeValue("gray.100", "black");
   return (
-    <Box bg={bgColor} minH={"var(--chakra-vh)"}>
-      <Header />
-      {children}
-      <Footer />
-    </Box>
+    <NetworkAvailabiltyCheck>
+      <Box bg={bgColor} minH={"var(--chakra-vh)"}>
+        <Header />
+        {children}
+        <Footer />
+      </Box>
+    </NetworkAvailabiltyCheck>
   );
 }
