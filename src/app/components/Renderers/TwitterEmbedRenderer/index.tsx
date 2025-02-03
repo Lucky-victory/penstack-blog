74eb776c0ce3c23ddf034b52a9edf10b488d7916
Lucky Victory <luckyvictory54@gmail.com>
2025-02-03 14:21:43 +0100

    Add Twitter embed functionality using react-twitter-embed; enhance rendering with loading placeholders and caption support

--- src/app/components/Renderers/TwitterEmbedRenderer/index.tsx
+++ src/app/components/Renderers/TwitterEmbedRenderer/index.tsx
@@ -9,9 +9,15 @@ import {
   Card,
   CardBody,
   Textarea,
+  Stack,
+  Skeleton,
+  SkeletonText,
+  HStack,
+  SkeletonCircle,
 } from "@chakra-ui/react";
 import { NodeViewProps, NodeViewWrapper } from "@tiptap/react";
 import { ChangeEvent, memo, useEffect } from "react";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 interface PenstackTwitterEmbedProps {
   isEditing?: boolean;
@@ -19,106 +25,65 @@ interface PenstackTwitterEmbedProps {
   updateAttributes?: (attrs: Record<string, any>) => void;
 }
 
-export const PenstackTwitterEmbed: React.FC<PenstackTwitterEmbedProps> = memo(
-  ({ node, isEditing = true, updateAttributes }) => {
-    const bgColor = useColorModeValue("gray.50", "gray.800");
-    const borderColor = useColorModeValue("gray.200", "gray.700");
-    const { colorMode } = useColorMode();
-    useEffect(() => {
-      const twitterScript = document.getElementById(
-        "penstack-twitter-embed-script"
-      );
+export const PenstackTwitterEmbed: React.FC<PenstackTwitterEmbedProps> = ({
+  node,
+  isEditing = true,
+  updateAttributes,
+}) => {
+  const bgColor = useColorModeValue("gray.50", "gray.800");
+  const borderColor = useColorModeValue("gray.200", "gray.700");
+  const { colorMode } = useColorMode();
 
-      if (!window?.twttr) {
-        const script = document.createElement("script");
-        script.id = "penstack-twitter-embed-script";
-        script.src = "https://platform.twitter.com/widgets.js";
-        script.async = true;
-        document.body.appendChild(script);
+  const handleCaptionChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    updateAttributes?.({ caption: e.target.value });
+  };
 
-        script.onload = () => {
-          if (window.twttr) {
-            window.twttr.widgets.load();
-          }
-        };
-      } else {
-        window.twttr.widgets.load();
-      }
-
-      return () => {
-        const scriptToRemove = document.getElementById(
-          "penstack-twitter-embed-script"
-        );
-
-        if (scriptToRemove) {
-          scriptToRemove.remove();
-        }
-      };
-    }, []);
-
-    const handleCaptionChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-      updateAttributes?.({ caption: e.target.value });
-    };
-
-    if (!node?.attrs?.tweetId) return null;
-    const content = (
-      <Card maxW="full">
-        <CardBody>
-          <VStack align="stretch" spacing={3}>
-            <Box
-              className="twitter-tweet-container"
-              sx={{
-                "& .twitter-tweet": {
-                  margin: "0 auto !important",
-                },
-              }}
-            >
-              <div id={`tweet-${node.attrs.tweetId}`}>
-                <Box
-                  className="twitter-tweet"
-                  data-conversation="none"
-                  data-theme={colorMode === "dark" ? "dark" : "light"}
-                >
-                  <Link
-                    isExternal
-                    href={`https://twitter.com/x/status/${node.attrs.tweetId}`}
-                  >
-                    <Spinner />
-                  </Link>
-                </Box>
-              </div>
+  if (!node?.attrs?.tweetId) return null;
+  const content = (
+    <Card maxW="full">
+      <CardBody>
+        <Stack spacing={2}>
+          <Box>
+            <TwitterTweetEmbed
+              tweetId={node.attrs.tweetId}
+              options={{ theme: colorMode }}
+              placeholder={
+                <Stack>
+                  <HStack>
+                    <SkeletonCircle size="10" />
+                    <SkeletonText noOfLines={1} width="60%" rounded={"xl"} />
+                  </HStack>
+                  <SkeletonText noOfLines={4} spacing="4" rounded={"xl"} />
+                  <Skeleton height="150px" mt={4} rounded={"xl"} />
+                </Stack>
+              }
+            />
+          </Box>
+          {isEditing && (
+            <Textarea
+              rows={2}
+              border="none"
+              borderBottom="1px solid"
+              borderColor="gray.300"
+              placeholder="Add caption (optional)"
+              value={node.attrs.caption || ""}
+              variant=""
+              onChange={handleCaptionChange}
+              resize="none"
+            />
+          )}
+          {!isEditing && node.attrs.caption && (
+            <Box fontSize="lg" fontWeight="bold">
+              {node.attrs.caption}
             </Box>
-            {isEditing && (
-              <Textarea
-                rows={2}
-                border="none"
-                borderBottom="1px solid"
-                borderColor="gray.300"
-                placeholder="Add caption (optional)"
-                value={node.attrs.caption || ""}
-                variant=""
-                onChange={handleCaptionChange}
-                resize="none"
-              />
-            )}
-            {!isEditing && node.attrs.caption && (
-              <Box fontSize="lg" fontWeight="bold">
-                {node.attrs.caption}
-              </Box>
-            )}
-          </VStack>
-        </CardBody>
-      </Card>
-    );
-    return (
-      <>{isEditing ? <NodeViewWrapper>{content}</NodeViewWrapper> : content}</>
-    );
-  }
-);
+          )}
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+  return (
+    <>{isEditing ? <NodeViewWrapper>{content}</NodeViewWrapper> : content}</>
+  );
+};
 
-declare global {
-  interface Window {
-    twttr: any;
-  }
-}
 PenstackTwitterEmbed.displayName = "TwitterEmbed";
