9b5017fb8199771ce1f184f3537ececa6bd8c4ea
Lucky Victory <luckyvictory54@gmail.com>
2025-03-02 14:50:49 +0100

    Refactor permission checks in usePermissions hook and middleware for improved readability and performance

--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -4,12 +4,6 @@ import { usePermissionsStore } from "../state/permissions";
 export const usePermissions = (requiredPermission: TPermissions) => {
   const permissions = usePermissionsStore((state) => state.permissions);
   const isLoading = usePermissionsStore((state) => state.isLoading);
-  let hasPermission: boolean = false;
-  if (permissions?.length) {
-    const found = permissions.find(
-      (permission) => permission === requiredPermission
-    );
-    hasPermission = !!found;
-  }
+  const hasPermission = permissions.includes(requiredPermission);
   return { hasPermission, loading: isLoading };
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,10 @@
 import { withAuth } from "next-auth/middleware";
-import { ClientPermissionHandler } from "./lib/auth/client-permission-handler";
 
 export default withAuth({
   callbacks: {
     authorized: ({ token }) => {
       if (!token) return false;
-      return ClientPermissionHandler.checkPermission({
-        requiredPermission: "dashboard:access",
-        email: token?.email || "",
-        appendSiteUrl: true,
-      }).then((res) => res.hasPermission);
+      return token.permissions.includes("dashboard:access");
     },
   },
 
